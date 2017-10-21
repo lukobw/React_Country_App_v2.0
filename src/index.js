4ab4e75bc2ecdb53b46a975f90e983c70f0b854d
Lucas <viper20@poczta.fm>
2017-10-21 11:34:06 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import { Router, hashHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import store from './store/store';
 import routes from './routes';
-// import {getCountries} from './actions/actions-countries';
+import {getCountries} from './actions/actions-countries';
+import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes}/>
+        <div>
+             <Router history={hashHistory} routes={routes}/>
+             <DevTools/>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
 
-// store.dispatch(getCountries());
-// console.log(store.getState());
+store.dispatch(getCountries());
+console.log(store.getState());
